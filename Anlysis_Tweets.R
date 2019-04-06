a07317e00f34a5f294ea938945f76dfce6811d70
paquinho89 <37455186+paquinho89@users.noreply.github.com>
2019-04-06 16:30:35 +0200

    Add files via upload

--- Anlysis_Tweets.R
+++ Anlysis_Tweets.R
@@ -1,32 +1,78 @@
 # install rtweet from CRAN
-install.packages("rtweet")
+# Here we are checking if the package is installed
+if(!require("rtweet")){
+  # If the package is not in the system then it will be install
+  install.packages("rtweet", dependencies = TRUE)
+  # Here we are loading the package
+  library("rtweet")
+}
 #isntall tmap from the maps
-install.packages('tmap')
-#install ggplot2
-install.packages(
-  "ggplot2",
-  repos = c("http://rstudio.org/_packages",
-            "http://cran.rstudio.com")
-)
+if(!require("tmap")){
+  # If the package is not in the system then it will be install
+  install.packages("tmap", dependencies = TRUE)
+  # Here we are loading the package
+  library("tmap")
+}
+# Here we are checking if the package is installed
+if(!require("ggplot2")){
+  # If the package is not in the system then it will be install
+  install.packages(
+    "ggplot2",
+    repos = c("http://rstudio.org/_packages",
+              "http://cran.rstudio.com")
+  )
+  # Here we are loading the package
+  library("ggplot2")
+}
 #This is for the maps and geolocations
-install.packages('googleway')
+# Here we are checking if the package is installed
+if(!require("googleway")){
+  # If the package is not in the system then it will be install
+  install.packages("googleway", dependencies = TRUE)
+  # Here we are loading the package
+  library("googleway")
+}
 #install stringi
-install.packages("stringi", repos="http://cran.rstudio.com/", dependencies=TRUE)
-install.packages("yaml")
-install.packages("mapsapi")
-install.packages("devtools")
-install.packages('googleLanguageR')
-
-library(rtweet)
-library(ggplot2)
-library(ggmap)
-library(tidyverse)
-library(dplyr)
-library (googleway)
-require(rvest)
-library(mapsapi)
-library(googleLanguageR)
-
+# Here we are checking if the package is installed
+if(!require("stringi")){
+  # If the package is not in the system then it will be install
+  install.packages("stringi", repos="http://cran.rstudio.com/", dependencies=TRUE)
+  # Here we are loading the package
+  library("stringi")
+}
+#install yaml
+# Here we are checking if the package is installed
+if(!require("yaml")){
+  # If the package is not in the system then it will be install
+  install.packages("yaml", repos="http://cran.rstudio.com/", dependencies=TRUE)
+  # Here we are loading the package
+  library("yaml")
+}
+#install mapsapi
+# Here we are checking if the package is installed
+if(!require("mapsapi")){
+  # If the package is not in the system then it will be install
+  install.packages("yaml", repos="http://cran.rstudio.com/", dependencies=TRUE)
+  # Here we are loading the package
+  library("mapsapi")
+}
+#install devtools
+# Here we are checking if the package is installed
+if(!require("devtools")){
+  # If the package is not in the system then it will be install
+  install.packages("devtools", repos="http://cran.rstudio.com/", dependencies=TRUE)
+  # Here we are loading the package
+  library("devtools")
+}
+#install googleLanguageR
+# Here we are checking if the package is installed
+if(!require("googleLanguageR")){
+  # If the package is not in the system then it will be install
+  install.packages("googleLanguageR", repos="http://cran.rstudio.com/", dependencies=TRUE)
+  # Here we are loading the package
+  library("googleLanguageR")
+}
+#####-------------------------------------------------------------
 #The GSK tweets are going to be downloaded
 ## load rtweet package
 #For more information regarding the tweets, see the below link:
@@ -43,28 +89,78 @@ create_token(
 my_key<-'AIzaSyC9EWMz-hClrRCdgaLwvl_N4B355d-CdAA'
 
 #Get the tweets which talk about GSK, choosing the country and the recent tweets
+#Collemos tamÈn os rtweets.
+#E tamÈn collemos solo os tweets que est·n en espaÒol
 GSK_tweets <- search_tweets(
-  "GSK", type='recent', n = 50, include_rts = FALSE, 
-  lookup_coords(address='spain', apikey = my_key)
+  "GSK", type='recent', n = 10000, include_rts =TRUE, 
+  lookup_coords(address='spain', apikey = my_key), lang="es"
 )
+View(GSK_tweets)
 #To see where the tweets come from.
 #The city is obtained
 location <-GSK_tweets[['location']]
-View(location)
 location_city<-strsplit(location, split=",")
 for (i in 1:length(location_city)){
   GSK_tweets[['location_city']][i] <-location_city [[i]][1]
 }
+
+#########################################################################
+#MISSING VALUES
+#For the cells with no location, we are going to set Madrid as the default value
+
+for(i in 1:nrow(GSK_tweets)){
+  GSK_tweets[['location_city']][i]<-if (is.na(GSK_tweets[[i,89]])){
+    'Madrid'} else {
+      GSK_tweets[[i,89]]}
+}
+View(GSK_tweets)
+############################################################################
 #The latitude and longitude of the city is obtained (coordenate_y and coordenate_x)
 for (i in 1:length(location)){
   GSK_tweets[['coordenates_y']][i] <- geocode_coordinates(google_geocode(address=GSK_tweets[['location_city']][i], key=my_key, simplify=TRUE))[1]
   GSK_tweets[['coordenates_x']][i] <- geocode_coordinates(google_geocode(address=GSK_tweets[['location_city']][i], key=my_key, simplify=TRUE))[2]
 }
-View(GSK_tweets)
-labels(GSK_tweets)
 
-geocode_coordinates(google_geocode(address='Madrid, Spain', key=my_key, simplify=TRUE))
 
+
+
+#The following code is to not overlap tweets and be able to visualize all of the tweets
+#This is to not have tweets in the same place. If there are tweets in the same place,
+#they overlap themselves and not all tweets can be visualized.
+#To see the column names.
+colnames(GSK_tweets)
+#To get the number of rows
+number_of_rows <- nrow(GSK_tweets)
+
+#We go from 2 to the lenght of data frame.
+#We check if the value is equal to the previous one, If it is equal, we add 1 and i/10.
+for (i in 2:length(location)) {
+  if (GSK_tweets[[i,90]]==GSK_tweets[[i-1,90]]){
+    print(GSK_tweets[[i,90]]+i/900)} else {
+      print(GSK_tweets[[i,90]])}
+}
+
+#We put the new coordenates in another column named 'coordenates_y_1'.
+#I mean, the previous code is placed in a new column in the data frame.
+#We add i in order to not have the same coordentes. Because if there are lot of equal
+#values, and we add 1, we will still have the same values. For this reason,
+#we need to add a different value each time. For this reason, we sum i, as it is
+#a value which change over the loop.
+for (i in 2:length(location)){
+  GSK_tweets[['coordenates_y_1']][1]<- GSK_tweets[[1,90]]
+  GSK_tweets[['coordenates_y_1']][i]<-if (GSK_tweets[[i,90]]==GSK_tweets[[i-1,90]]){
+      GSK_tweets[[i,90]]+i/900} else {
+        GSK_tweets[[i,90]]}
+}
+#We creaate another column where we copy the same value as in the coordenates_x_1 column
+#This is because in some cases some coordenates appear as vector (c(52.1458,54.2214,14.251)).
+#and if we copy the values in the new column, just copy a value from vector and 
+#there is no problem ahead.
+for (n in 1:length(location)){
+  GSK_tweets[['coordenates_x_1']][n]<- GSK_tweets[[n,91]]
+}
+
+View (GSK_tweets)
 #####################################################
 #Temos que traducir o text para que me pille ben as cidades
 #Pra darte de alta teste que ler estas duas paxinas
@@ -73,121 +169,105 @@ geocode_coordinates(google_geocode(address='Madrid, Spain', key=my_key, simplify
 text <- "to administer medicince to animals is frequently a very difficult matter, and yet sometimes it's necessary to do so"
 gl_translate(text, target = "es")$translatedText
 ?Startup
-
-##################################################
-#Representing the map of the country
-google_map(key = my_key, data = GSK_tweets) %>%
-  add_markers(lat = "coordenates_y", lon = "coordenates_x", info_window = 'name')
 #################################################
-#Vou a intentar creaar outro tipo de mapas mais molÛn co tema do pop up mais guapo
+#Representing the map.
 library(leaflet)
-leaflet() %>% addTiles() %>% addMarkers(lng= ~coordenates_y, lat= ~coordenates_x)
-#m=addTiles(m)
-
+#Convertimos as coordenadas_x e as coordenadas_y a formao numÈrico para poder represenetar
+#os markers no mapa.
+#Representamos o mapa e despois engadimos os markers para ver de donde son os tweets.
 leaflet(data = GSK_tweets) %>% addTiles() %>%
-  addMarkers(lng = GSK_tweets[1,"coordenates_y"], lat= GSK_tweets[1,"coordenates_x"])
-#, popup = ~as.character(mag), label = ~as.character(mag))
-
-GSK_tweets[1,"coordenates_y"]
-
-View(GSK_tweets[c("coordenates_y","coordenates_x")])
-#https://rstudio.github.io/leaflet/popups.html
-#https://rstudio.github.io/leaflet/markers.html
-
-
-
-
-
-
-
-
-View(tram_stops)
-
-geocode_coordinates(GSK_tweets_location)
-
-google_map(key = my_key, location='spain'   )
-
-get_map(m)
-
-
-
-
-
-
-
-
-
-
+  addMarkers(lng = as.numeric(GSK_tweets$coordenates_x_1), lat= as.numeric(GSK_tweets$coordenates_y_1),
+             popup = ~as.character(text),label = ~as.character(name))
+########################################################################
 #WORDCLOUD
-#Ver esta paxina http://www.sthda.com/english/wiki/text-mining-and-word-cloud-fundamentals-in-r-5-simple-steps-you-should-know
-install.packages("tm")  # for text mining
-install.packages("SnowballC") # for text stemming
-install.packages("wordcloud") # word-cloud generator 
-install.packages("RColorBrewer") # color palettes
-# Load
-library("tm")
-library("SnowballC")
-library("wordcloud")
-library("RColorBrewer")
+# install tidytext
+# Here we are checking if the package is installed
+if(!require("tidytext")){
+  # If the package is not in the system then it will be install
+  install.packages("tidytext", dependencies = TRUE)
+  # Here we are loading the package
+  library("tidytext")
+}
+# install dplyr
+# Here we are checking if the package is installed
+if(!require("dplyr")){
+  # If the package is not in the system then it will be install
+  install.packages("dplyr", dependencies = TRUE)
+  # Here we are loading the package
+  library("dplyr")
+}
+# install stringr
+# Here we are checking if the package is installed
+if(!require("stringr")){
+  # If the package is not in the system then it will be install
+  install.packages("stringr", dependencies = TRUE)
+  # Here we are loading the package
+  library("stringr")
+}
+require(devtools)
+# install wordcloud
+# Here we are checking if the package is installed
+if(!require("wordcloud")){
+  # If the package is not in the system then it will be install
+  install.packages("wordcloud", dependencies = TRUE)
+  # Here we are loading the package
+  library("wordcloud")
+}
 
-txt <- tweets$text
-View(txt)
-docs <- Corpus(VectorSource(txt))
-View(docs)
-toSpace <- content_transformer(function (x , pattern ) gsub(pattern, " ", x))
-docs <- tm_map(docs, toSpace, "/")
-docs <- tm_map(docs, toSpace, "@")
-docs <- tm_map(docs, toSpace, "\\|")
-#Esto √© para eliminar os emoticonos
-docs <- tm_map(docs, toSpace, "[^\x01-\x7F]")
+#Look at tweets
+head(GSK_tweets)
+dim(GSK_tweets)
+GSK_tweets$text
 
+#Unnest the words - code via Tidy Text
+#O que estamos facendo e crear unha fila por palabra.
+GSK_tweetsTable <- GSK_tweets %>% 
+  unnest_tokens(word, text)
+View(GSK_tweetsTable)
 
+#Esto son unhas librerÌas de palabras, que valen para que me eliminen esas palabras
+#que non aportan valor dos tweets.
+View(stopwords("spanish"))
+#Busca para que vale esto.
+spanish_stop_words <- bind_rows(stop_words,
+                                data_frame(word = tm::stopwords("spanish"),
+                                           lexicon = "custom"))
+#Esto traime tÛdolos valores que est·n na tabla GSK_tweetsTable e que non est·n na
+#tabla de stop_words. Se os valores est·n nas duas tablas, pois non mos trae.
+GSK_tweetsTable <- GSK_tweetsTable %>%
+  anti_join(spanish_stop_words)
 
-################################################################
-#Vamos a intentar identificar e eliminar os art√≠culos, preposicions e demais cousas.
-#Botalle un ollo a esta paxina que est√° interesante
-#https://cran.r-project.org/web/packages/udpipe/vignettes/udpipe-usecase-postagging-lemmatisation.html
+View(GSK_tweetsTable)
+#do a word count
+GSK_tweetsTable <- GSK_tweetsTable %>%
+  count(word, sort = TRUE) 
+View(GSK_tweetsTable)
 
-library("openNLP")
-library(NLP)
+#Remove other nonsense words
+GSK_tweetsTable <-GSK_tweetsTable %>%
+  filter(!word %in% c('t.co', 'https'))
+View(GSK_tweetsTable)
 
-install.packages("spacyr")
-library("spacyr")
-sentence <- "My name is Francisco"
-##############################################################################################
-
-# Convert the text to lower case
-docs <- tm_map(docs, content_transformer(tolower))
-# Remove numbers
-docs <- tm_map(docs, removeNumbers)
-# Remove english common stopwords
-docs <- tm_map(docs, removeWords, stopwords("english"))
-# Remove your own stop word
-# specify your stopwords as a character vector
-docs <- tm_map(docs, removeWords, c("https", "tco")) 
-# Remove punctuations
-docs <- tm_map(docs, removePunctuation)
-# Eliminate extra white spaces
-docs <- tm_map(docs, stripWhitespace)
-# Text stemming
-# docs <- tm_map(docs, stemDocument)
-
-dtm <- TermDocumentMatrix(docs)
-m <- as.matrix(dtm)
-v <- sort(rowSums(m),decreasing=TRUE)
-d <- data.frame(word = names(v),freq=v)
-head(d, 10)
-
-set.seed(1234)
-wordcloud(words = d$word, freq = d$freq, min.freq = 1,
-          max.words=500, random.order=FALSE, rot.per=0.35, 
+#Imprimimos la nube de palabras
+wordcloud(words = GSK_tweetsTable$word, freq = GSK_tweetsTable$n, min.freq = 10,
+          max.words=500, random.order=TRUE, rot.per=0.1, 
           colors=brewer.pal(8, "Dark2"))
 
-#The frequency of the first 10 frequent words are plotted :
-barplot(d[1:10,]$freq, las = 2, names.arg = d[1:10,]$word,
-        col ="lightblue", main ="Most frequent words",
-        ylab = "Word frequencies")
+################################################################################
+#See the people with the most number of followers which is talking about GSK
+data.frame(GSK_tweets$name, sort(GSK_tweets$followers_count, decreasing=TRUE))
+##See the people with the most number of friends which is talking about GSK
+data.frame(GSK_tweets$name, sort(GSK_tweets$friends_count, decreasing=TRUE))
 
+
+
+
+
+
+
+
+
+#ESTO POSIBLEMENTE O ELIMINES
 ##Vamos a identificar se as palabras son positivas, negativas e que sentimentos nos
 ##transmiten.
 library(tidytext)
